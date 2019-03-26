59961d6db5f6549ba80efda30ce204b0bd1cff95
AnsonChans <601943401@qq.com>
2019-03-26 14:04:59 +0800

    动态组件

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,17 +1,25 @@
+import { HeroProfileComponent } from './hero-profile.component';
+import { HeroJobAdComponent } from './hero-job-ad.component';
+import { AdDirective } from './ad.directive';
+import { AdComponent } from './ad.component';
+import { AdBannerComponent } from './ad-banner.component';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-
+import { AdService } from './ad.service';
 @NgModule({
+  providers: [AdService],
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }])
   ],
-  declarations: [Tab3Page]
+  entryComponents: [ HeroJobAdComponent, HeroProfileComponent ],
+  declarations: [Tab3Page,AdDirective,AdBannerComponent,HeroJobAdComponent, HeroProfileComponent],
+  bootstrap: [ Tab3Page ]
 })
 export class Tab3PageModule {}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { AdItem } from './ad-item';
+import { Component, OnInit } from '@angular/core';
+import { AdService } from './ad.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit {
+  
+  ads:AdItem[];
+  constructor(private adService:AdService){}
+
+  ngOnInit(){
+    this.ads = this.adService.getAds();
+  }
+
+}
